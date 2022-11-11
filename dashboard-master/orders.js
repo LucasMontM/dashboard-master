c552c7905c54d47a87ba0339fb20e41ad9c688f8
Lucas Mont-Mor <lucasmontmro3@gmail.com>
2022-11-11 12:00:19 -0300

    intial commit

---
+++ dashboard-master/orders.js
@@ -0,0 +1,49 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARAVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Drone with Camera',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'GPS 3k Drone',
+        productNumber: '96996',
+        paymentStatus: 'Due',
+        shipping:'Pendind'
+    },
+    {
+        productName: 'DJI Air 24',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+
+
+
+
+]
